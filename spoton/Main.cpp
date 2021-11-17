52233a3a96efdfd3165cab714884903709e8a1de
Turbosmurfen <turbosmurfen@protonmail.com>
2021-11-17 11:38:42 +0100

    Added license
    Updated the sourcecode with Marcus lonnbergs license. Also updated the code to halt after executing some commands.

--- spoton/Main.cpp
+++ spoton/Main.cpp
@@ -1,3 +1,30 @@
+/* Play/Pause, Next, Previous
+
+Copyright 2010 Marcus Lönnberg
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/* Announce and Creator
+
+Copyright 2021 Turbosmurfen
+
+Licensed under the Creative Commons License, version 4.0
+https://creativecommons.org/licenses/by-nc/4.0/
+
+*/
+
+
 #include <cstdio>
 #include <windows.h>
 #include <string>
@@ -10,8 +37,10 @@
 #pragma comment(linker, "/EXPORT:next=_next@24")
 #pragma comment(linker, "/EXPORT:prev=_prev@24")
 using namespace std;
+
 string title{"0"};
 HWND hWNd{};
+
 static BOOL CALLBACK enumWindowCallback(HWND hWnd, LPARAM lparam) {
     string wClass{};
     int length = GetWindowTextLength(hWnd);
@@ -67,19 +96,19 @@ extern "C" int __stdcall pause(HWND mWnd, HWND aWnd, CHAR * data, char* parms, B
 {
     EnumWindows(enumWindowCallback, NULL);
     if (hWNd) SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PLAY_PAUSE * 0x10000);
-    return 2;
+    return 0;
 }
 
 extern "C" int __stdcall next(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
     EnumWindows(enumWindowCallback, NULL);
     if (hWNd) SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_NEXTTRACK * 0x10000);
-    return 2;
+    return 0;
 }
 
 extern "C" int __stdcall prev(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
     EnumWindows(enumWindowCallback, NULL);
     if (hWNd) SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PREVIOUSTRACK * 0x10000);
-    return 2;
+    return 0;
 }
