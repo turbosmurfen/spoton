74b71ed2504ee8eff059320dafe965e7aea6404e
Turbosmurfen <turbosmurfen@protonmail.com>
2021-11-19 00:38:11 +0100

    Only execute enumwindows if changed
    Only execute enumwindows if changed.

--- spoton/Main.cpp
+++ spoton/Main.cpp
@@ -30,6 +30,7 @@ https://creativecommons.org/licenses/by-nc/4.0/
 #include <string>
 #include <vector>
 #include <iostream>
+#include <chrono>
 #define WIN32_LEAN_AND_MEAN
 #pragma comment(linker, "/EXPORT:song=_song@24")
 #pragma comment(linker, "/EXPORT:creator=_creator@24")
@@ -41,86 +42,95 @@ https://creativecommons.org/licenses/by-nc/4.0/
 #pragma comment(linker, "/EXPORT:rplay=_rplay@24")
 using namespace std;
 
-string title{""};
-int status_{0};
+string title{};
+string lastTitle{};
+int status_{ 0 };
+int statusLast{ 0 };
 HWND hWNd{};
+HWND lasthWNd{};
 
 void readData(string input, HWND hWnd) {
 
-    if (!input.empty()) {
+	if (!input.empty()) {
 
-        if (input == "Advertisement" || title.find("-") == 0) {
-            status_ = 2;
-            hWNd = hWnd;
-        }
-        else if (input == "Spotify Premium" || input == "Spotify Free") {
-            status_ = 1;
-            hWNd = hWnd;
-        }
-        else {
-            hWNd = hWnd;
-            title = input;
-            status_ = 3;
-        }
-    }
-    else {
-        status_ = 0;
-    }
+		if (input == "Advertisement" || title.find("-") == 0) {
+			status_ = 2;
+			hWNd = hWnd;
+		}
+		else if (input == "Spotify Premium" || input == "Spotify Free") {
+			status_ = 1;
+			hWNd = hWnd;
+		}
+		else {
+			hWNd = hWnd;
+			title = input;
+			status_ = 3;
+		}
+	}
+	else {
+		status_ = 0;
+	}
 }
 
 static BOOL CALLBACK enumWindowCallback(HWND hWnd, LPARAM lparam) {
-    string wClass{};
-    int length = GetWindowTextLength(hWnd);
-    char* buffer = new char[length + 1];
-    GetWindowTextA(hWnd, buffer, length + 1);
-    string wTitle(buffer);
-    char* buffer0 = new char[256];
-    GetClassNameA(hWnd, buffer0, 256);
-    wClass = buffer0;
+	string wClass{};
+	int length = GetWindowTextLength(hWnd);
+	char* buffer = new char[length + 1];
+	GetWindowTextA(hWnd, buffer, length + 1);
+	string wTitle(buffer);
+	char* buffer0 = new char[256];
+	GetClassNameA(hWnd, buffer0, 256);
+	wClass = buffer0;
 
-    if (IsWindowVisible(hWnd) && length != 0 && wClass == "Chrome_WidgetWin_0") {
-        readData(wTitle, hWnd);
-    }
+	if (IsWindowVisible(hWnd) && length != 0 && wClass == "Chrome_WidgetWin_0") {
+		readData(wTitle, hWnd);
 
-    return TRUE;
+	}
+
+	return TRUE;
 }
 
 
 //Writes out title of the song
 extern "C" int __stdcall song(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    EnumWindows(enumWindowCallback, NULL);
-    if (status_ != 0) {
-        char* cstr = new char[title.size() + 1];
-        title.copy(cstr, title.size() + 1);
-        cstr[title.size()] = '\0';
-        strcpy_s(data, strlen(cstr) + 1, cstr);
-    }
-    return 3;
+	if (title != lastTitle || title.empty()) {
+		EnumWindows(enumWindowCallback, NULL);
+		lastTitle = title;
+	}
+	char* cstr = new char[title.size() + 1];
+	title.copy(cstr, title.size() + 1);
+	cstr[title.size()] = '\0';
+	strcpy_s(data, strlen(cstr) + 1, cstr);
+
+	return 3;
 }
 
 extern "C" int __stdcall creator(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    char cby[] = "Created By: Turbosmurfen";
-    strcpy_s(data, strlen(cby) + 1, cby);
-    return 3;
+	char cby[] = "Created By: Turbosmurfen";
+	strcpy_s(data, strlen(cby) + 1, cby);
+	return 3;
 }
 
 /* Check for status
 * 0 - Spotify is not running
 * 1 - Spotify is paused
 * 2 - Spotfy is playing Advertisement
-* 3 - Spotify is playing 
+* 3 - Spotify is playing
 */
 extern "C" int __stdcall status(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    EnumWindows(enumWindowCallback, NULL);
-    string input = std::to_string(status_);
-    char* cstr = new char[input.size() + 1];
-    input.copy(cstr, input.size() + 1);
-    cstr[input.size()] = '\0';
-    strcpy_s(data, strlen(cstr) + 1, cstr);
-    return 3;
+	if (status_ != statusLast || !status_) {
+		EnumWindows(enumWindowCallback, NULL);
+		statusLast = status_;
+	}
+	string input = std::to_string(status_);
+	char* cstr = new char[input.size() + 1];
+	input.copy(cstr, input.size() + 1);
+	cstr[input.size()] = '\0';
+	strcpy_s(data, strlen(cstr) + 1, cstr);
+	return 3;
 }
 
 // Here starts the commands for Spotify Media Player.
@@ -129,44 +139,53 @@ extern "C" int __stdcall status(HWND mWnd, HWND aWnd, CHAR * data, char* parms, 
 //Execute pause track for Spotify Media Player
 extern "C" int __stdcall pause(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    EnumWindows(enumWindowCallback, NULL);
-    if (hWNd && status_ == 3) {
-        SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PLAY_PAUSE * 0x10000);
-    }
-    return 0;
+	EnumWindows(enumWindowCallback, NULL);
+	if (hWNd && status_ == 3) {
+		SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PLAY_PAUSE * 0x10000);
+	}
+	return 0;
 }
 //Execute play track for Spotify Media Player
 extern "C" int __stdcall play(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    EnumWindows(enumWindowCallback, NULL);
-    if (hWNd && status_ == 1) {
-        SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PLAY_PAUSE * 0x10000);
-    }
-    return 0;
+	EnumWindows(enumWindowCallback, NULL);
+	if (hWNd && status_ == 1) {
+		SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PLAY_PAUSE * 0x10000);
+	}
+	return 0;
 }
 //Execute next track for Spotify Media Player
 extern "C" int __stdcall next(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    EnumWindows(enumWindowCallback, NULL);
-    if (hWNd) SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_NEXTTRACK * 0x10000);
-    return 0;
+	if (hWNd != lasthWNd || !hWNd) {
+		EnumWindows(enumWindowCallback, NULL);
+		lasthWNd = hWNd;
+	}
+	if (hWNd) SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_NEXTTRACK * 0x10000);
+	return 0;
 }
 
 //Execute replay track for Spotify Media Player
 extern "C" int __stdcall rplay(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    EnumWindows(enumWindowCallback, NULL);
-    if (hWNd) SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PREVIOUSTRACK * 0x10000);
-    return 0;
+	if (hWNd != lasthWNd || !hWNd) {
+		EnumWindows(enumWindowCallback, NULL);
+		lasthWNd = hWNd;
+	}
+	if (hWNd) SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PREVIOUSTRACK * 0x10000);
+	return 0;
 }
 
 //Execute previous track for Spotify Media Player
 extern "C" int __stdcall prev(HWND mWnd, HWND aWnd, CHAR * data, char* parms, BOOL show, BOOL nopause)
 {
-    EnumWindows(enumWindowCallback, NULL);
-    if (hWNd) {
-        SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PREVIOUSTRACK * 0x10000);
-        SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PREVIOUSTRACK * 0x10000);
-    }
-    return 0;
+	if (hWNd != lasthWNd || !hWNd) {
+		EnumWindows(enumWindowCallback, NULL);
+		lasthWNd = hWNd;
+	}
+	if (hWNd) {
+		SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PREVIOUSTRACK * 0x10000);
+		SendMessage(hWNd, WM_APPCOMMAND, 0, APPCOMMAND_MEDIA_PREVIOUSTRACK * 0x10000);
+	}
+	return 0;
 }
